0cf09e0d423bf21c8f7ddaf8bc6885d93eae372f
Tad Hardesty <tad@platymuus.com>
2021-01-15 05:27:29 -0800

    Update Dockerfile for CBT (#56175)
    Follow-up to fix the failing Docker CI on master.
    
    - Reorganize the entire Dockerfile to be more readable
    - Inline the tgstation/byond Dockerfile into our own, so we can change the base distro at will
    - Also allows us to trash the dependencies.sh<->Dockerfile hack
    - Use 32-bit libs on a 64-bit distro so that we can download and run recent 64-bit Node binaries
    - Call tools/build/build rather than DreamMaker directly
    Conflicts:
    	.editorconfig
    	dependencies.sh

--- dependencies.sh
+++ dependencies.sh
@@ -4,11 +4,16 @@
 #Final authority on what's required to fully build the project
 
 # byond version
+<<<<<<< HEAD
 # Extracted from the Dockerfile. Change by editing Dockerfile's FROM command.
 LIST=($(sed -n 's/.*byond:\([0-9]\+\)\.\([0-9]\+\).*/\1 \2/p' Dockerfile))
 export BYOND_MAJOR=${LIST[0]}
 export BYOND_MINOR=${LIST[1]}
 unset LIST
+=======
+export BYOND_MAJOR=513
+export BYOND_MINOR=1533
+>>>>>>> c9aa81d... Update Dockerfile for CBT (#56175)
 
 #rust_g git tag
 export RUST_G_VERSION=0.4.7
